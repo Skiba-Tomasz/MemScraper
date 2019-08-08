385abe46283b39795c876db568d66ff6f30be497
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2019-08-09 01:17:51 +0200

    Refactored. Works in background, one method in public returns mems on x page. Need for handling exceptions in future.

--- memscraper/src/main/java/memscraper/MemScraper.java
+++ memscraper/src/main/java/memscraper/MemScraper.java
@@ -1,6 +1,8 @@
 package memscraper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,45 +10,60 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class MemScraper {
-	private String domainURL = "https://jbzdy.net/str/1";
-	private Document jsDoc = null;
+	private final String domainURL = "https://jbzdy.net/str/";
 	
-	public static void main(String[] args) {
-		MemScraper memScraper = new MemScraper();
-	}
 	
-	public MemScraper(){
+
+	public List<MemObject> loadMemsFromPage(int pageNumber) {
+		Elements memPosts = null;
 		try {
-			jsDoc = Jsoup.connect(domainURL).get();
-			
-			Elements postFeed = jsDoc.select("div#container-left").select("div.content.nobottompad");
-			System.out.println("Feed: "+  postFeed.text());
-			Elements memPosts = new Elements();
-			for(Element element : postFeed) {
-				//postFeed;
-				if(element.select("article.resource-object ") != null)memPosts.addAll(element.select("article.resource-object "));
-			}
-			System.out.println(memPosts.size());
-			
-			for(Element memPost : memPosts) {
-				//Nazwa
-				System.out.println(memPost.select("div.content-info").select("div.title").text());
-				//Tagi
-				System.out.println(memPost.select("div.content-info").select("div.info").select("div.tags").text());
-				//ImageURL
-				String imagesInPost = memPost.select("div.content-info").select("div.media").select("div.image.rolled").select("img").attr("src");
-				System.out.println(imagesInPost);
-				//Plusy
-				System.out.println(memPost.select("div.content-info").select("div.content-actions").select("div").select("span").text());
-				
-				System.out.println("=====");
-			}
+			Document jsDoc = Jsoup.connect(domainURL+pageNumber).get();	
+			Elements postContainer = jsDoc.select("div#container-left").select("div.content.nobottompad");
+			memPosts = getMemPosts(postContainer);				
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//Handle this later
 			e.printStackTrace();
 		}
-		
+		return scrapeMemsFromPage(memPosts);
 	}
 
+	private List<MemObject> scrapeMemsFromPage(Elements memPosts) {
+		List<MemObject> memsFromPosts = new ArrayList<>();
+		for(Element post : memPosts) {
+			MemObject mem = new MemObject.Builder().setTitle(getMemPostTitle(post))
+													.setTags(getMemPostTags(post))
+													.setRating(getMemPostRating(post))
+													.setUrl(getMemPostImageUrl(post)).build();
+			memsFromPosts.add(mem);
+		}
+		return memsFromPosts;
+	}
+
+	private Elements getMemPosts(Elements currentPostContainer) {
+		Elements memPosts = new Elements();
+		for(Element element : currentPostContainer) {
+			if(element.select("article.resource-object ") != null)memPosts.addAll(element.select("article.resource-object "));
+		}
+		return memPosts;
+	}
+	
+	private String getMemPostTitle(Element post) {
+		return post.select("div.content-info").select("div.title").text();
+	}
+	
+	private String[] getMemPostTags(Element post) {
+		String tagString = post.select("div.content-info").select("div.info").select("div.tags").text();
+		String tagStringWithoutFirstHash = tagString.substring(1);
+		String[] tags = tagStringWithoutFirstHash.split("#");
+		return tags;
+	}
+	
+	private String getMemPostImageUrl(Element post) {
+		return post.select("div.content-info").select("div.media").select("div.image.rolled").select("img").attr("src");
+	}
+
+	private int getMemPostRating(Element post) {
+		return Integer.parseInt(post.select("div.content-info").select("div.content-actions").select("div").select("span").text());
+	}
 }
 		
