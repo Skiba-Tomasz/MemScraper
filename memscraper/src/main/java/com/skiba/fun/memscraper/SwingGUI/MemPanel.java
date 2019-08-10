ba25f2eb0c3104442bb9654947d02e070d923aea
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2019-08-10 20:33:28 +0200

    Centered mem images. Changes to layout. Giving up on centering video.

--- memscraper/src/main/java/com/skiba/fun/memscraper/SwingGUI/MemPanel.java
+++ memscraper/src/main/java/com/skiba/fun/memscraper/SwingGUI/MemPanel.java
@@ -1,9 +1,13 @@
 package com.skiba.fun.memscraper.SwingGUI;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JPanel;
-
+import javax.swing.SwingConstants;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,10 +22,10 @@ import com.skiba.fun.memscraper.JBZDmemscraper.MemObject;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-
 import javafx.util.Duration;
 
 
@@ -34,60 +38,73 @@ public class MemPanel extends JPanel {
 
 	public MemPanel(MemObject mem) {
 		
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new BorderLayout());
 		setBackground(Color.DARK_GRAY);
+		
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+		infoPanel.setBackground(Color.darkGray);
 		
 		topSeparatorLabel = new JLabel(" ");
 		topSeparatorLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		add(topSeparatorLabel);
+		infoPanel.add(topSeparatorLabel);
 		
 		titleLabel = new JLabel(" " + mem.getTitle() + " [" + mem.getRating() + "]");
 		titleLabel.setForeground(Color.red);
 		titleLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		add(titleLabel);
+		infoPanel.add(titleLabel);
 		
 		tagLabel = new JLabel(" " + Arrays.toString(mem.getTags()));
 		tagLabel.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		tagLabel.setForeground(Color.red);
-		add(tagLabel);
+		infoPanel.add(tagLabel);
 		
 		bottomSeparatorLabel = new JLabel(" ");
 		bottomSeparatorLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		add(bottomSeparatorLabel);
+		infoPanel.add(bottomSeparatorLabel);
 		
-		setSize(700, getMinimumSize().height);
+		add(infoPanel, BorderLayout.NORTH);
+		
 		
 		switch(mem.getType()) {
 		case IMAGE:
-			imageDisplayLabel = new JLabel("");
-			imageDisplayLabel.setIcon(mem.getImage());
-			//imageDisplayLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-			add(imageDisplayLabel);
+			JPanel imagePanel = new JPanel();
+			imagePanel.setLayout(new BorderLayout());
+			imagePanel.setBackground(Color.DARK_GRAY);
+			imageDisplayLabel = new JLabel(mem.getImage(), JLabel.CENTER);
+			imageDisplayLabel.setBackground(Color.white);
+			imageDisplayLabel.setSize(new Dimension(getWidth(), imageDisplayLabel.getMinimumSize().height));
+			imagePanel.add(imageDisplayLabel, BorderLayout.CENTER);
+			add(imagePanel, BorderLayout.CENTER);
 			break;
 		case VIDEO:
 			getVideo(mem);
 		}
-		
+		setSize(700, getMinimumSize().height);
+		validate();
 	}
 	
 	public void getVideo(MemObject mem) {
          String url = mem.getContentURL();
          JFXPanel jfxPanel = new JFXPanel();
+         jfxPanel.setAlignmentX(RIGHT_ALIGNMENT);
+         JPanel wraperPanel = new JPanel();
+         wraperPanel.setLayout(new BorderLayout());
          JPanel videoPanel = new JPanel();
-         videoPanel.setLayout(new BoxLayout(videoPanel, BoxLayout.Y_AXIS));
+         videoPanel.setLayout(new BorderLayout());
          videoPanel.setBackground(Color.DARK_GRAY);
-         videoPanel.setPreferredSize(mem.getVideoSize());
-         videoPanel.setMaximumSize(mem.getVideoSize());
 
          Media media = new Media(url);
          MediaPlayer player = new MediaPlayer(media);
          MediaView view = new MediaView(player);
+         
          Group g = new Group(view);
-         Scene s = new Scene(g);         
+         Scene s = new Scene(g, mem.getVideoSize().width, mem.getVideoSize().height);
          jfxPanel.setScene(s);
-         videoPanel.add(jfxPanel);
+
+         videoPanel.add(jfxPanel, BorderLayout.CENTER);
          JButton play = new JButton("Play!");
-         videoPanel.add(play);
+         videoPanel.add(play, BorderLayout.SOUTH);
          play.addActionListener(new ActionListener() {
 			
 			@Override
@@ -96,7 +113,7 @@ public class MemPanel extends JPanel {
 				player.play();
 			}
 		});
-         videoPanel.validate();
-         add(videoPanel);
+         wraperPanel.add(videoPanel, BorderLayout.CENTER);
+         add(wraperPanel, BorderLayout.CENTER);
 	}
 }
