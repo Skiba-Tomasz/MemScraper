e0e02c64c9c045a5271b2d9ae1d29c0c58c1ee56
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2019-08-11 16:26:20 +0200

    Cleaned up code.

--- memscraper/src/main/java/com/skiba/fun/memscraper/SwingGUI/MemPanelImage.java
+++ memscraper/src/main/java/com/skiba/fun/memscraper/SwingGUI/MemPanelImage.java
@@ -4,22 +4,35 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.skiba.fun.memscraper.JBZDmemscraper.MemObject;
 
 public class MemPanelImage extends JPanel{
+	private JLabel imageDisplayLabel;
 	
 	public MemPanelImage(MemObject mem) {
-		//JPanel imagePanel = new JPanel();
-		setLayout(new BorderLayout());
-		setBackground(Color.DARK_GRAY);
-		JLabel imageDisplayLabel = new JLabel(mem.getImage(), JLabel.CENTER);
-		imageDisplayLabel.setBackground(Color.white);
-		imageDisplayLabel.setSize(new Dimension(getWidth(), imageDisplayLabel.getMinimumSize().height));
+		initializeLayout();
+		imageDisplayLabel = setImageToLabelView(mem.getImage());
 		add(imageDisplayLabel, BorderLayout.CENTER);
-		//add(imagePanel, BorderLayout.CENTER);
 	}
 
+	public MemPanelImage(ImageIcon icon) {
+		initializeLayout();
+		imageDisplayLabel = setImageToLabelView(icon);
+		add(imageDisplayLabel, BorderLayout.CENTER);
+	}
+	
+	private void initializeLayout() {
+		setLayout(new BorderLayout());
+		setBackground(Color.DARK_GRAY);
+	}
+	
+	private JLabel setImageToLabelView(ImageIcon image) {
+		JLabel label = new JLabel(image, JLabel.CENTER);
+		label.setSize(new Dimension(getWidth(), label.getMinimumSize().height));
+		return label;
+	}
 }
--- memscraper/src/main/java/com/skiba/fun/memscraper/SwingGUI/MemPanelVideo.java
+++ memscraper/src/main/java/com/skiba/fun/memscraper/SwingGUI/MemPanelVideo.java
@@ -3,9 +3,7 @@ package com.skiba.fun.memscraper.SwingGUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -19,86 +17,74 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.util.Duration;
 
-public class MemPanelVideo extends JPanel{
-
+public class MemPanelVideo extends JPanel implements Runnable{
+	private MemObject mem;
+	private JButton play;
+	private JButton stop;
+	private JButton restart;
+	private JButton mute;
+	private JPanel mediaControllPanel;
+	private JPanel videoPlayerPanel;
+	private JFXPanel videoPanel;
+	private MediaPlayer player;
+	
 	public MemPanelVideo(MemObject mem) {
-		Thread videoInitializer = new Thread() {
-			public void run() {
-				setBackground(Color.DARK_GRAY);
-		        String url = mem.getContentURL();
-		        JFXPanel jfxPanel = new JFXPanel();
-		        jfxPanel.setAlignmentX(RIGHT_ALIGNMENT);
-
-		        JPanel videoPanel = new JPanel();
-		        videoPanel.setLayout(new BorderLayout());
-		        videoPanel.setBackground(Color.DARK_GRAY);
-
-		        Media media = new Media(url);
-		        MediaPlayer player = new MediaPlayer(media);
-		        MediaView view = new MediaView(player);
-		        
-		        Group g = new Group(view);
-		        Scene s = new Scene(g, mem.getVideoSize().width, mem.getVideoSize().height);
-		        jfxPanel.setScene(s);
-
-		        videoPanel.add(jfxPanel, BorderLayout.CENTER);
-		        
-		        JPanel buttonWrapperPanel = new JPanel();
-		        
-		        buttonWrapperPanel.setLayout(new BorderLayout());
-		        
-		        JPanel buttonPanel = new JPanel();
-		        buttonPanel.setBackground(Color.DARK_GRAY);
-		        setAlignmentX(CENTER_ALIGNMENT);
-		        
-		        JButton play = new JButton("Play");
-		        play.setAlignmentX(CENTER_ALIGNMENT);
-		        buttonPanel.add(play);
-		        play.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						
-						player.play();
-					}
-				});
-		        
-		        JButton stop = new JButton("Stop");
-		        buttonPanel.add(stop);
-		        stop.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						player.pause();
-					}
-				});
-		        
-		        JButton restart = new JButton("Resart");
-		        buttonPanel.add(restart);
-		        restart.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						player.seek(Duration.ZERO);
-					}
-				});
-		        
-		        JButton mute = new JButton("Mute");
-		        buttonPanel.add(mute);
-		        mute.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						player.setMute(!player.isMute());
-					}
-				});
-		        buttonWrapperPanel.add(buttonPanel, BorderLayout.CENTER);
-		        videoPanel.add(buttonWrapperPanel, BorderLayout.SOUTH);
-		        add(videoPanel, BorderLayout.CENTER);
-			}
-		};
-		videoInitializer.start();
+		this.mem = mem;
+		setBackground(Color.DARK_GRAY);
+		Thread videoProcesor = new Thread(this);
+		videoProcesor.start();
 	}
 	
-	
+	public void run() {
+        initializeVideoPlayer();
+        add(videoPlayerPanel, BorderLayout.CENTER);
+	}
+
+	private void initializeVideoPlayer() {
+		videoPlayerPanel = new JPanel();
+        videoPlayerPanel.setLayout(new BorderLayout());
+        videoPlayerPanel.setBackground(Color.DARK_GRAY);     
+        initializeVideoPanel();
+        videoPlayerPanel.add(videoPanel, BorderLayout.CENTER);
+        initializeMediaControllPanel(player);   
+        videoPlayerPanel.add(mediaControllPanel, BorderLayout.SOUTH);
+	}
+
+	private void initializeVideoPanel() {
+		String source = mem.getContentURL();
+        videoPanel = new JFXPanel();
+        videoPanel.setAlignmentX(RIGHT_ALIGNMENT);
+        Media media = new Media(source);
+        player = new MediaPlayer(media);
+        MediaView view = new MediaView(player);    
+        Group g = new Group(view);
+        Scene s = new Scene(g, mem.getVideoSize().width, mem.getVideoSize().height);
+        videoPanel.setScene(s);
+	}
+
+	private void initializeMediaControllPanel(MediaPlayer player) {
+		mediaControllPanel = new JPanel();
+        mediaControllPanel.setLayout(new BorderLayout());    
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.DARK_GRAY);
+        setAlignmentX(CENTER_ALIGNMENT);
+        play = new JButton("Play");
+        play.setAlignmentX(CENTER_ALIGNMENT);
+        buttonPanel.add(play);
+        stop = new JButton("Stop");
+        buttonPanel.add(stop);    
+        restart = new JButton("Resart");
+        buttonPanel.add(restart);   
+        mute = new JButton("Mute");
+        buttonPanel.add(mute);      
+        addButtonMediaPlayerControllsActions(player);    
+        mediaControllPanel.add(buttonPanel, BorderLayout.CENTER);
+	}
+
+	private void addButtonMediaPlayerControllsActions(MediaPlayer player) {
+		play.addActionListener((ActionEvent e) -> player.play());			
+        stop.addActionListener((ActionEvent e) -> player.pause());
+        restart.addActionListener((ActionEvent e) -> player.seek(Duration.ZERO));
+        mute.addActionListener((ActionEvent e) ->player.setMute(!player.isMute()));
+	}
 }
